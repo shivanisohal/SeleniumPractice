2de4fa0e1a9ff621b01302489386f26335cfcc99
Shivanisohal <sohalshivani2@gmail.com>
2025-05-22 09:03:53 +0530

    selenium First program

---
+++ src/test/java/SeleniumPracticeWork/SeleniumBasics/SeleniumBasic_1.java
@@ -0,0 +1,15 @@
+package SeleniumPracticeWork.SeleniumBasics;
+
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
+public class SeleniumBasic_1 {
+
+    @Test
+    public void OpenLoginPage()
+    {
+        EdgeDriver edgeDriver=new EdgeDriver();
+        edgeDriver.get("https://app.vwo.com");
+        System.out.println(edgeDriver.getTitle());
+    }
+}
